1b9c21dff489e0a362c147b5d8e261d03fef704d
dgrudzinski <dgrudzinski@dgrudzinskis-MacBook-Pro.local>
2024-08-09 14:49:40 +0200

    Add Actuator

--- src/main/java/com/example/spring6restmvc/config/SpringSecurityConfig.java
+++ src/main/java/com/example/spring6restmvc/config/SpringSecurityConfig.java
@@ -1,15 +1,31 @@
 package com.example.spring6restmvc.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
 
     @Bean
+    @Order(1)
+    public SecurityFilterChain actuatorSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry
+                                               -> authorizationManagerRequestMatcherRegistry.requestMatchers(
+                        EndpointRequest.toAnyEndpoint()).permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> {
                     authorize.anyRequest().authenticated();
