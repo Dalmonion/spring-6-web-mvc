8b1d4d655566c0fcd274cf89809605a86f82fbe7
dgrudzinski <dgrudzinski@dgrudzinskis-MacBook-Pro.local>
2024-09-09 10:22:27 +0200

    Add create order dtos

---
+++ src/main/java/com/example/spring6restmvc/model/BeerOrderCreateDTO.java
@@ -0,0 +1,19 @@
+package com.example.spring6restmvc.model;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@Builder
+public class BeerOrderCreateDTO {
+
+    private String customerRef;
+
+    @NotNull
+    private UUID customer;
+    private Set<BeerOrderLineCreateDTO> beerOrderLines;
+}
